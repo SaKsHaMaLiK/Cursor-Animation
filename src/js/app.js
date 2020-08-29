4ef2a9b41e136e8ad2ce751a59888553ad254ce8
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-29 06:05:10 +0530

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -75,3 +75,10 @@ $('.section li:nth-child(7)').on('mouseover', function(event) {
 $('.section li:nth-child(7)').on('mouseout', function(event) {		
     $('body').removeClass('preload');
 });
+// loding
+$('.section li:nth-child(8)').on('mouseover', function(event) {				
+    $('body').addClass('preload1');
+});				
+$('.section li:nth-child(8)').on('mouseout', function(event) {		
+    $('body').removeClass('preload1');
+});
